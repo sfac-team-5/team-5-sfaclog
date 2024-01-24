1b702aa8048bb4f7fc2b5f72276ef05491eb577b
geonwooPark <white0581@naver.com>
2024-01-24 17:57:25 +0900

    Chore: config 파일 설정

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ['@repo/ui'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /.svg$/,
