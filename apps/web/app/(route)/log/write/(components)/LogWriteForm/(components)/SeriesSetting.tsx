ebab25a787c410c63a553648da59339caf6ce459
kangkyeongseo <kks_big@naver.com>
2024-01-25 16:22:20 +0900

    Feat: SeriesSetting 컴포넌트 생성

--- apps/web/app/(route)/log/write/(components)/LogWriteForm/(components)/SeriesSetting.tsx
+++ apps/web/app/(route)/log/write/(components)/LogWriteForm/(components)/SeriesSetting.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
 
 function SeriesSetting() {
+  const { data: session } = useSession();
+  const [series, setSeries] = useState();
+
+  useEffect(() => {
+    if (!session) return;
+
+    async function fetchData(id: string) {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/series/${id}`,
+        );
+        const data = await response.json();
+        setSeries(data);
+      } catch (error) {}
+    }
+    fetchData(session.user.id);
+  }, [session]);
+
   return <div>SeriesSetting</div>;
 }
 
