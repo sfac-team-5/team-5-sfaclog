797034dd1c282ac7a3d7fe262e792c0f73435145
kangkyeongseo <kks_big@naver.com>
2024-01-25 16:20:38 +0900

    Feat: ContentInput 컴포넌트 생성

--- apps/web/app/(route)/log/write/(components)/LogWriteForm/(components)/ContentInput.tsx
+++ apps/web/app/(route)/log/write/(components)/LogWriteForm/(components)/ContentInput.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+'use client';
+import React, { useMemo, useRef } from 'react';
+import ReactQuill, { Quill } from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import 'react-quill/dist/quill.bubble.css';
+import { ImageResize } from 'quill-image-resize-module-ts';
+Quill.register('modules/ImageResize', ImageResize);
 
-function ContentInput() {
-  return <div>ContentInput</div>;
+interface ContentInputProps {
+  content: string;
+  setContent: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default ContentInput;
+export default function ContentInput({
+  content,
+  setContent,
+}: ContentInputProps) {
+  const quillRef = useRef<any>();
+
+  const onChange = (newContent: string) => {
+    setContent(newContent);
+  };
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          [{ size: ['small', false, 'large', 'huge'] }],
+          [{ color: [] }, { background: [] }],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{ list: 'ordered' }, { list: 'bullet' }, { align: [] }],
+          ['image', 'link', 'code-block'],
+        ],
+      },
+      clipboard: {
+        matchVisual: false,
+      },
+    };
+  }, []);
+
+  return (
+    <ReactQuill
+      ref={quillRef}
+      value={content}
+      theme={'snow'}
+      onChange={onChange}
+    />
+  );
+}
