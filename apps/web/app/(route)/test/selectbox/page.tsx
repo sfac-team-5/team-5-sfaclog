86ce9a84d67e5da7b3c2d57c717095ab547e6126
jaeha kim <kimjh0069@naver.com>
2024-01-27 22:50:59 +0900

    Feat: ui-Selectbox 추가

---
+++ apps/web/app/(route)/test/selectbox/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+import React from 'react';
+import { Selectbox } from '@repo/ui/SelectBox';
+import { IconTaillessArrowUp, IconTaillessArrowDown } from '@public/svgs';
+const selectList = [
+  { value: 'Wade Cooper' },
+  { value: 'Arlene Mccoy' },
+  { value: 'Devon Webb' },
+  { value: 'Tom Cook' },
+  { value: 'Tanya Fox' },
+  { value: 'Hellen Schmidt' },
+];
+export default function page() {
+  return (
+    <div className='ml-14'>
+      <Selectbox
+        onChange={data => console.log(data)}
+        IconArrowDown={<IconTaillessArrowDown className='stroke-neutral-90' />}
+        IconArrowUp={<IconTaillessArrowUp className='stroke-neutral-90' />}
+        selectList={selectList}
+        width='long'
+        placeholder='무엇이 불편하셨나요?'
+        label='라벨, 옵션들 추후 폰트 수정해야함'
+      />
+      <div>
+        <Selectbox
+          onChange={data => console.log(data)}
+          IconArrowDown={
+            <IconTaillessArrowDown className='stroke-neutral-90' />
+          }
+          IconArrowUp={<IconTaillessArrowUp className='stroke-neutral-90' />}
+          selectList={selectList}
+          width='short'
+          label='라벨'
+        />
+      </div>
+    </div>
+  );
+}
--- apps/web/public/svgs/index.ts
+++ apps/web/public/svgs/index.ts
@@ -1,3 +1,12 @@
 import IconNext from './next.svg';
-
-export { IconNext };
+import IconTaillessArrowUp from './arrows/tailless_arrow_up.svg';
+import IconTaillessArrowDown from './arrows/tailless_arrow_down.svg';
+import IconTaillessArrowRight from './arrows/tailless_arrow_right.svg';
+import IconTaillessArrowLeft from './arrows/tailless_arrow_left.svg';
+export {
+  IconNext,
+  IconTaillessArrowUp,
+  IconTaillessArrowDown,
+  IconTaillessArrowRight,
+  IconTaillessArrowLeft,
+};
