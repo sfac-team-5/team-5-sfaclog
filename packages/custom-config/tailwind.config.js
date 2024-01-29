ca82a36bda8bd022d9454b5af437fbebc5a3c63d
kangkyeongseo <kks_big@naver.com>
2024-01-29 15:22:04 +0900

    Chore: Tailwind config 설정 수정 및 추가

--- packages/custom-config/tailwind.config.js
+++ packages/custom-config/tailwind.config.js
@@ -8,6 +8,15 @@ const customConfig = {
     '../../packages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    container: {
+      screens: {
+        lg: '1440px',
+      },
+      padding: {
+        lg: '240px',
+      },
+      center: true,
+    },
     extend: {
       fontFamily: {
         pretendard: ['var(--font-pretendard)'],
@@ -15,25 +24,90 @@ const customConfig = {
       screens: {
         sm: '360px',
         md: '768px',
-        lg: '1024px',
+        lg: '1440px',
       },
       boxShadow: {
         custom: '0 4px 16px 0 rgba(7, 46, 120, 0.14)',
       },
+      fontSize: {
+        H0M32: [
+          '32px',
+          { fontWeight: 500, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        H1B24: [
+          '24px',
+          { fontWeight: 700, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        H1M24: [
+          '24px',
+          { fontWeight: 500, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        H1R24: [
+          '24px',
+          { fontWeight: 400, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        H2B20: [
+          '20px',
+          { fontWeight: 700, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        H2M20: [
+          '20px',
+          { fontWeight: 500, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        H2R20: [
+          '20px',
+          { fontWeight: 400, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        H3B18: [
+          '18px',
+          { fontWeight: 700, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        H3M18: [
+          '18px',
+          { fontWeight: 500, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        H3R18: [
+          '18px',
+          { fontWeight: 400, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        B1B16: [
+          '16px',
+          { fontWeight: 700, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        B1M16: [
+          '16px',
+          { fontWeight: 500, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        B1R16: [
+          '16px',
+          { fontWeight: 400, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        B2B14: [
+          '14px',
+          { fontWeight: 700, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        B2M14: [
+          '14px',
+          { fontWeight: 500, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        B2R14: [
+          '14px',
+          { fontWeight: 400, lineHeight: '150%', letterSpacing: 0 },
+        ],
+        B3B12: [
+          '12px',
+          { fontWeight: 700, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        B3M12: [
+          '12px',
+          { fontWeight: 500, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+        B3R12: [
+          '12px',
+          { fontWeight: 400, lineHeight: 'normal', letterSpacing: 0 },
+        ],
+      },
       colors: {
-        primary: {
-          100: '#0059FF',
-          90: '#196AFF',
-          80: '#337AFF',
-          70: '#4C8BFF',
-          60: '#669BFF',
-          50: '#7FACFF',
-          40: '#99BDFF',
-          30: '#B2CDFF',
-          20: '#CCDEFF',
-          10: '#E5EEFF',
-          5: '#F5F8FF',
-        },
         neutral: {
           100: '#030303',
           90: '#1A1A1A',
@@ -47,21 +121,46 @@ const customConfig = {
           10: '#E6E6E6',
           5: '#F3F3F3',
         },
+        brand: {
+          100: '#0059FF',
+          90: '#196AFF',
+          80: '#337AFF',
+          70: '#4C8BFF',
+          60: '#669BFF',
+          50: '#7FACFF',
+          40: '#99BDFF',
+          30: '#B2CDFF',
+          20: '#CCDEFF',
+          10: '#E5EEFF',
+          5: '#F5F8FF',
+        },
+        highlight: {
+          warning: '#FF0000',
+          success: '#07A320',
+        },
+        text: {
+          primary: '#1A1A1A',
+          secondary: '#4D4D4D',
+          point: '#196AFF',
+          gray: '#999999',
+          white: '#FFFFFF',
+          waring: '#FF000',
+          success: '#07A320',
+        },
+        state: {
+          90: '#196AFF',
+          70: '#4C8BFF',
+          10: '#E5EEFF',
+          popup: '#E5E9F2',
+        },
         stroke: {
           50: '#7FACFF',
           30: '#B3B3B3',
-          10: '#E6E6E6',
-          5: '#F3F3F3',
         },
         background: {
-          100: '#666666',
-          50: 'var(--background-50-opacity, rgba(0, 0, 0, 0.30))',
-          5: '#F8F8F9',
-          blue: '#F5F8FF',
-        },
-        system: {
-          warning: '#FF0000',
-          success: '#07A320',
+          white: '#FFFFFF',
+          10: '#E6E6E6',
+          5: '#F3F3F3',
         },
       },
     },
