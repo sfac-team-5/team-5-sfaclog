4e07a56c0674a6e0e95cf5ff0805dafed4718657
geonwooPark <white0581@naver.com>
2024-01-31 18:54:46 +0900

    Feat: 로그인 페이지 제작

--- apps/web/app/(route)/(auth)/login/page.tsx
+++ apps/web/app/(route)/(auth)/login/page.tsx
@@ -1,68 +1,12 @@
-'use client';
 import React from 'react';
-import { useForm } from 'react-hook-form';
-
-import { submitAction } from './action';
-import { useRouter } from 'next/navigation';
-import UsernameInput from './(components)/UsernameInput';
-import PasswordInput from './(components)/PasswordInput';
-
-export interface LoginInputType {
-  username: string;
-  password: string;
-}
+import LoginForm from './(components)/LoginForm';
+import LoginFormHeader from './(components)/LoginFormHeader';
 
 export default function page() {
-  const router = useRouter();
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginInputType>({
-    defaultValues: {
-      username: '',
-      password: '',
-    },
-  });
-
-  const handleFormSubmit = async (data: LoginInputType) => {
-    const { loginSuccess } = await submitAction(data);
-
-    if (loginSuccess) {
-      router.refresh();
-      alert('로그인되었습니다.');
-      router.back();
-    } else {
-      console.log('다시 로그인해 주세요.');
-    }
-  };
-
-  const usernameRegister = register('username', {
-    required: '아이디를 입력해 주세요.',
-  });
-
-  const passwordRegister = register('password', {
-    required: '비밀번호를 입력해 주세요.',
-  });
-
   return (
-    <form onSubmit={handleSubmit(handleFormSubmit)}>
-      <div className='relative mb-6'>
-        <UsernameInput register={usernameRegister} errors={errors} />
-        {errors.username && (
-          <div className='absolute text-red-500'>{errors.username.message}</div>
-        )}
-      </div>
-      <div className='relative mb-6'>
-        <PasswordInput register={passwordRegister} errors={errors} />
-        {errors.password && (
-          <div className='absolute text-red-500'>{errors.password.message}</div>
-        )}
-      </div>
-      <button type='submit' className='border-brand-50 w-28 border-4'>
-        제출
-      </button>
-    </form>
+    <main className='mx-auto flex min-h-[650px] w-[400px] flex-col justify-center pb-[85px] pt-[50px]'>
+      <LoginFormHeader />
+      <LoginForm />
+    </main>
   );
 }
