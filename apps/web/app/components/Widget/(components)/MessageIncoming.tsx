48ac26824bbeaf324f10436cee39d192faad2f98
Gahee Kim <soprue@naver.com>
2024-02-01 17:12:34 +0900

    Style: 메시지 말풍선 컴포넌트 스타일 수정

--- apps/web/app/components/Widget/(components)/MessageIncoming.tsx
+++ apps/web/app/components/Widget/(components)/MessageIncoming.tsx
@@ -1,7 +1,7 @@
 export function MessageIncoming({ message }: { message: string }) {
   return (
-    <div className='mb-4 flex w-full items-end justify-start gap-1'>
-      <div className='bg-neutral-5 text-B3R12 text-neutral-90 min-w-5 max-w-[250px] rounded-md rounded-bl-none p-2'>
+    <div className='mb-[15px] flex w-full items-end justify-start gap-1'>
+      <div className='bg-neutral-5 text-B3R12 text-neutral-90 flex min-h-[34px] min-w-5 max-w-[250px] items-center rounded-md rounded-bl-none p-2'>
         {message}
       </div>
       <div className='text-neutral-40 text-[8px]'>
