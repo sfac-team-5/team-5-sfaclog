1ba4d743d999f7350b7442534de16e8ae64ad555
kangkyeongseo <kks_big@naver.com>
2024-02-01 23:01:39 +0900

    Feat: 커뮤니티 생성

--- apps/web/app/api/community/route.ts
+++ apps/web/app/api/community/route.ts
@@ -1,5 +1,17 @@
+import { auth } from '@/auth';
 import { NextRequest, NextResponse } from 'next/server';
 import PocketBase from 'pocketbase';
+
+interface CommunityDataType {
+  author: string | undefined;
+  category: FormDataEntryValue | null;
+  title: FormDataEntryValue | null;
+  tags: string;
+  content: FormDataEntryValue | null;
+  views: number;
+  likes: number;
+  isVisibility: boolean;
+}
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
@@ -18,6 +30,38 @@ export async function GET(request: NextRequest) {
       return NextResponse.json(records.items);
     }
   } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'An unexpected error occurred',
+      },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  const session = await auth();
+  const formData = await request.formData();
+  const jsonTags = JSON.stringify(formData.getAll('tags'));
+
+  try {
+    const data: CommunityDataType = {
+      author: session?.user.id,
+      category: formData.get('series'),
+      title: formData.get('title'),
+      tags: jsonTags,
+      content: formData.get('content'),
+      views: 0,
+      likes: 0,
+      isVisibility: true,
+    };
+    console.log(data);
+    const pb = new PocketBase(`${process.env.POCKETBASE_URL}`);
+    const newCommunity = await pb.collection('communities').create(data);
+
+    return NextResponse.json(newCommunity);
+  } catch (error) {
+    // console.log(error);
     return NextResponse.json(
       {
         message: 'An unexpected error occurred',
