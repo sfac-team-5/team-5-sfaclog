fafda292f94f15f886899d0bdf4ad56aa49dc9f2
Gahee Kim <soprue@naver.com>
2024-02-05 16:55:39 +0900

    Update: 섹션 헤더 더보기 버튼 TextButton으로 변경

--- apps/web/app/components/SectionHeader.tsx
+++ apps/web/app/components/SectionHeader.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { IconTaillessArrowRightBlue } from '@repo/ui/Icon';
+import TextButton from './TextButton';
 
 interface SectionHeaderProps {
   title: string;
@@ -11,15 +12,7 @@ export function SectionHeader({ title, more }: SectionHeaderProps) {
   return (
     <div className='flex items-center justify-between'>
       <p className='text-H1M24'>{title}</p>
-      {more && (
-        <Link
-          href={{ pathname: more }}
-          className='text-B2R14 flex items-center justify-center gap-2 px-3 py-[6px]'
-        >
-          <span>더보기</span>
-          <IconTaillessArrowRightBlue width={15} />
-        </Link>
-      )}
+      {more && <TextButton link='/login' />}
     </div>
   );
 }
