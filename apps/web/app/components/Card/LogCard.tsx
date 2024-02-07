3cfdf42e1e7d628f4ffc7c86fe853f129a7e792c
geonwooPark <white0581@naver.com>
2024-02-07 13:54:02 +0900

    Chore: 메인 페이지 카드 컴포넌트에 링크 추가

--- apps/web/app/components/Card/LogCard.tsx
+++ apps/web/app/components/Card/LogCard.tsx
@@ -7,6 +7,7 @@ import { CardViews } from './(components)/CardViews';
 import { CardLikes } from './(components)/CardLikes';
 import { CardDate } from './(components)/CardDate';
 import { LogType } from '@/types';
+import Link from 'next/link';
 
 interface LogCardProps {
   log: LogType;
@@ -21,40 +22,42 @@ export function LogCard({ log }: LogCardProps) {
     : '사용자명';
 
   return (
-    <CardBox type='log'>
-      <div className='h-[280px] w-full overflow-hidden rounded-md'>
-        {log.thumbnailUrl === '' ? (
-          <div className='bg-background-5 size-full' />
-        ) : (
-          <Image
-            src={log.thumbnailUrl}
-            width={0}
-            height={0}
-            sizes='100%'
-            className='size-full object-cover'
-            alt='thumbnail'
-          />
-        )}
-      </div>
-      <div className='w-full'>
-        <div className='mb-2 flex items-center justify-between'>
-          <CardCategory category={series} />
-          <CardDate date={log.created} />
+    <Link href={`/log/${log.id}`}>
+      <CardBox type='log'>
+        <div className='h-[280px] w-full overflow-hidden rounded-md'>
+          {log.thumbnailUrl === '' ? (
+            <div className='size-full bg-background-5' />
+          ) : (
+            <Image
+              src={log.thumbnailUrl}
+              width={0}
+              height={0}
+              sizes='100%'
+              className='size-full object-cover'
+              alt='thumbnail'
+            />
+          )}
         </div>
-        <div>
-          <CardTitle title={log.title} />
-        </div>
-        <div className='mt-5 flex w-full justify-between'>
-          <div className='flex items-center gap-2'>
-            <Avatar size={24} />
-            <span className='text-B3R12'>{nickname}</span>
+        <div className='w-full'>
+          <div className='mb-2 flex items-center justify-between'>
+            <CardCategory category={series} />
+            <CardDate date={log.created} />
           </div>
-          <div className='flex gap-3'>
-            <CardViews count={log.views} />
-            <CardLikes count={log.likes} />
+          <div>
+            <CardTitle title={log.title} />
+          </div>
+          <div className='mt-5 flex w-full justify-between'>
+            <div className='flex items-center gap-2'>
+              <Avatar size={24} />
+              <span className='text-B3R12'>{nickname}</span>
+            </div>
+            <div className='flex gap-3'>
+              <CardViews count={log.views} />
+              <CardLikes count={log.likes} />
+            </div>
           </div>
         </div>
-      </div>
-    </CardBox>
+      </CardBox>
+    </Link>
   );
 }
