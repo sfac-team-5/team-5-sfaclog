c4ed694fa54c3ecd5eb823c1d2eeccf77b8a4e29
Gahee Kim <soprue@naver.com>
2024-02-08 16:26:53 +0900

    Feat: 메시지 위젯

--- apps/web/app/components/Header/Navigation/NavMessage.tsx
+++ apps/web/app/components/Header/Navigation/NavMessage.tsx
@@ -1,10 +1,25 @@
+'use client';
+import { useState } from 'react';
+
 import { IconMessageLine } from '@repo/ui/Icon';
+import { MessageWidget } from '@/components/Widget/MessageWidget';
 
 export function NavMessage() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div>
-      <IconMessageLine className='cursor-pointer' />
-      {/* <MessageWidget /> */}
+    <div className='relative'>
+      <IconMessageLine
+        className='cursor-pointer'
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      />
+      {isOpen && <MessageWidget onClose={onClose} />}
     </div>
   );
 }
--- apps/web/app/components/Widget/MessageWidget.tsx
+++ apps/web/app/components/Widget/MessageWidget.tsx
@@ -7,15 +7,19 @@ import { WidgetHeader } from './WidgetHeader';
 import MessageBox from './(components)/MessageBox';
 import { MessageChat } from './(components)/MessageChat';
 
-export function MessageWidget() {
+interface MessageWidgetProps {
+  onClose: () => void;
+}
+
+export function MessageWidget({ onClose }: MessageWidgetProps) {
   const [isChatOpen, setIsChatOpen] = useState(false);
 
   const tabClasses =
     'ui-selected:font-bold ui-selected:border-b-2 ui-selected:border-neutral-90 border-neutral-30 flex h-[64px] w-1/3 cursor-pointer items-center justify-center border-b-[1px] px-[24px] py-[10px] text-[16px] outline-none duration-200 ease-in-out';
 
   return (
-    <div className='absolute right-0 z-[100] mt-4 h-[720px] w-[360px] rounded-md bg-white drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]'>
-      <WidgetHeader type={'메시지'} />
+    <div className='absolute right-0 z-[100] mt-4 h-[720px] w-[360px] overflow-hidden rounded-md bg-white drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]'>
+      <WidgetHeader type={'메시지'} onClose={onClose} />
 
       {/* <NoData type='메시지' /> */}
       <Tabs defaultSelectedTabIndex={0}>
