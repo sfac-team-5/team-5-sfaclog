8a021bfaf7dab5c96fad0796b4a0bdb8fa6df2c8
geonwooPark <white0581@naver.com>
2024-02-13 01:37:20 +0900

    Style: Mypage 프로필 영역 너비 고정

--- apps/web/app/components/Profile/(components)/ProfileContainer.tsx
+++ apps/web/app/components/Profile/(components)/ProfileContainer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function ProfileContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className='shadow-custom flex h-fit !w-[245px] flex-col rounded-md p-5'>
+    <div className='flex h-fit !w-[245px] shrink-0 flex-col rounded-md p-5 shadow-custom'>
       {children}
     </div>
   );
